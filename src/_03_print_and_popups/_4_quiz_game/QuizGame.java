c624bb59c684892598092a26c552ca0df1e568e9
league <league@imac-16.attlocal.net>
2023-03-04 11:26:40 -0800

    World Domination, QuizGame, AwesomeOrNot, and Scary Maze Game.

--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,66 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
+
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
+
+		// 2.  Ask the user a question 
+		String q1 = JOptionPane.showInputDialog("How many legs does a spider have? (answer in a number, ex: 0, 1, 2...)");
+		// 3.  Use an if statement to check if their answer is correct
+		if(q1.equals("8")){
+			// 4.  if the user's answer was correct, add one to their score 
+			score += 1;
+
+		}
+		else {
+			score -= 1;	
+
+
+		}
+
+
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		// Option: Subtract a point from their score for a wrong answer
+		String q2 = JOptionPane.showInputDialog("What is 20 squared? (answer in a number, ex: 0, 1, 2)");
+
+		if(q2.equals("400")) {
+
+			score += 1;
+
+		}
+		else {
+			score -= 1;	
+
+
+		}
+
+
+		String q3 = JOptionPane.showInputDialog("What is 1+2");
+		if(q3.equals("3")){
+
+			score += 1;
+
+		}
+		else {
+
+			score -= 1;
+
+
+		}
+
+
 		// After all the questions have been asked, tell the user their final score 
+JOptionPane.showMessageDialog(null, "Your final score is " + score + "!");
+	
+if(score == 3) {
+	JOptionPane.showMessageDialog(null, "You got the maximum score!");
 		
+		
+}
 	}
 }
--- src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
+++ src/_03_print_and_popups/_5_awesome_or_not/AwesomeOrNot.java
@@ -1,36 +1,61 @@
 package _03_print_and_popups._5_awesome_or_not;
 
+import java.awt.print.Printable;
 import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes everything below
 
-	
-		Random ran = new Random();    //This will be used below to make a random number. 
-		
-		// 2. Make a variable that will hold a random whole number
-	
-		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
-	
-		// 3. Print your variable to the console
-	
-		// 4. Get the user to enter something that they think is awesome
-	
-		// 5. If your variable is  0
-	
-			// -- tell the user whatever they entered is awesome!
-	
-		// 6. If your variable is  1
-	
-			// -- tell the user whatever they entered is ok.
-	
-		// 7. If your variable is  2
-	
-			// -- tell the user whatever they entered is boring.
-	
-		// 8. If your variable is  3
-	
-			// -- invent your own message to give to the user (be nice).
+	public static void main(String[] args) {
 
+		Random ran = new Random();    //This will be used below to make a random number. 
+
+		// 2. Make a variable that will hold a random whole number
+
+		int randomwhole;
+
+		// 3. Set your variable equal to a positive number less than 4 using     ran.nextInt(4); 
+
+		randomwhole = ran.nextInt(4);
+
+		// 3. Print your variable to the console (syso + cntrl space)
+
+		System.out.println(randomwhole);
+
+		// 4. Get the user to enter something that they think is awesome
+
+		JOptionPane.showInputDialog("Type something in that you think is awesome.");
+
+		// 5. If your variable is  0
+		if(randomwhole == 0) {	
+			// -- tell the user whatever they entered is awesome!
+			JOptionPane.showMessageDialog(null, "That's awesome!");
+
+
+		}
+		// 6. If your variable is  1
+		if(randomwhole == 1) {
+			// -- tell the user whatever they entered is ok.
+			JOptionPane.showMessageDialog(null, "That's OK...");
+
+		}
+		// 7. If your variable is  2
+		if(randomwhole == 2) {
+			// -- tell the user whatever they entered is boring.
+			JOptionPane.showMessageDialog(null, "That's boring.");
+
+
+		}
+		// 8. If your variable is  3
+		if(randomwhole == 3) {
+			// -- invent your own message to give to the user (be nice).
+			JOptionPane.showMessageDialog(null, "I guess that's pretty cool...");
+
+
+		}
+
+	}
 }
